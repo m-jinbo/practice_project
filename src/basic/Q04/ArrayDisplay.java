2c147cd540be66c5ea66cb4085d4d977a97dba97
LAPTOP-7A53TPMT\casss <m-jinbo@seassaist.co.jp>
2024-10-04 09:09:42 +0900

    add basic

---
+++ src/basic/Q04/ArrayDisplay.java
@@ -0,0 +1,14 @@
+package basic.Q04;
+
+public class ArrayDisplay {
+	public static void main(String[] args) {
+
+		String[] fruits = { "りんご", "ばなな", "みかん", "ぶどう", "すいか" };
+
+		for (String fruit : fruits) {
+
+			System.out.println(fruit);
+		}
+	}
+
+}
---
+++ src/basic/q01/Subtractloop.java
@@ -0,0 +1,15 @@
+package basic.q01;
+
+public class Subtractloop {
+	public static void main(String[] args) {
+
+		int number = 100;
+
+		while (number >= 0) {
+
+			System.out.println(number);
+			number--;//1減少する
+		}
+
+	}
+}
